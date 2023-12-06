a241bd855ce5415feb3efe158c3938d44a75f7aa
LexTheAries2209 <673661658@qq.com>
2023-12-06 16:39:52 +0800

    Initial Commit

---
+++ login/loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginApp.swift
+//  login
+//
+//  Created by 吴坤城 on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
